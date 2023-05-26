58e32e582c9c08f28e66974886c7965c68fc3442
Viet Cuong Nguyen <vietcuong179@gmail.com>
2023-05-26 17:19:29 +0700

    change ver 5

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=mypostgres.cb98ev2dyawy.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=cuongnv
+export AWS_BUCKET=cuongnv98
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
